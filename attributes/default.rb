62a23917a4d17cb1514335e8485123175b6e3c65
Adrian <adrian.hornsby@dreambroker.com>
2014-07-08 14:39:12 +0300

    adding client related configuration, buffer, header ..

--- attributes/default.rb
+++ attributes/default.rb
@@ -87,6 +87,10 @@ default['nginx']['types_hash_bucket_size'] = 64
 
 default['nginx']['proxy_read_timeout'] = nil
 default['nginx']['client_max_body_size'] = nil
+default['nginx']['client_header_timeout'] = nil
+default['nginx']['client_max_body_size'] = nil
 
 default['nginx']['proxy_cache_path'] = nil
 default['nginx']['proxy_temp_path'] = nil
+
+default['nginx']['send_timeout'] = nil
